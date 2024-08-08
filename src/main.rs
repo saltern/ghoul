6c6eabe321edb7050eb239b481ae5948c650eb57
sALT <93945502+saltern@users.noreply.github.com>
2024-08-08 17:15:55 -0300

    BMP support, multithreaded directory processing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
+use std::thread;
+use std::fs::ReadDir;
+use std::ffi::OsStr;
 use std::path::PathBuf;
+use std::time::Instant;
 
 pub mod shared_types;
 pub mod param_validator;
@@ -40,7 +44,10 @@ pub fn main() {
 	let parameters: Parameters = opt_parameters.unwrap();
 	
 	if parameters.directory_mode {
+		println!("Working...");
+		let instant = Instant::now();
 		process_directory(parameters);
+		println!(" in {}ms.", instant.elapsed().as_millis());
 	}
 	
 	else {
@@ -52,21 +59,22 @@ pub fn main() {
 pub fn help_message() {
 	println!();
 	println!("Tool for handling GGXX AC+R sprites.");
-	println!("Can convert and reindex PNG-, RAW-, and BIN-format sprites.");
+	println!("Can convert and reindex PNG-, RAW-, BIN-, and BMP-format sprites.");
 	println!();
 	println!("Usage:");
-	println!("    ghoul -i <input file>");
+	println!("    ghoul -i <input file> [-f format] [-o output] [-p/-c] [-r] [-u] [-w] [-l]");
 	println!();
 	println!("To process full directories, use an asterisk as the input file name (e.g. 'path/*.png').");
 	println!();
-	println!("    Available parameters:");
-	println!("        -f <format>       Convert sprites (formats: 'png', 'raw', 'bin')");
-	println!("        -o <path>         Set output path, defaults to the current directory if not specified");
-	println!("        -p <pal file>     If output format is 'png', color sprites using this .act palette");
-	println!("        -r                Reindex output sprites");
-	println!("        -u                Output uncompressed sprites (BIN only)");
-	println!("        -w                Overwrite pre-existing files");
-	println!("        -l                List processed files");
+	println!("Available parameters:");
+	println!("    -f or -format  <format>      Convert sprites (formats: 'png', 'raw', 'bin', 'bmp')");
+	println!("    -o or -output  <path>        Set output path, defaults to the current directory if not specified");
+	println!("    -p or -palette <pal file>    If output format is 'png' or 'bmp', color sprites using this .act palette");
+	println!("    -c or -palcopy               If converting from PNG to BMP or BMP to PNG, copy source palette to output");
+	println!("    -r or -reindex               Reindex output sprites");
+	println!("    -u or -uncompressed          Output uncompressed sprites (BIN only)");
+	println!("    -w or -overwrite             Overwrite pre-existing files");
+	println!("    -l or -list                  List processed files");
 	println!();
 	println!("When an output path is not specified, the current working directory will be used.");
 }
@@ -75,21 +83,18 @@ pub fn help_message() {
 fn process_file(parameters: Parameters) {
 	if parameters.verbose {
 		match parameters.source_path.file_name() {
-			Some(name) => println!("Processing '{}'...", name.to_str().unwrap()),
+			Some(name) => println!("Processing '{}'", name.to_str().unwrap()),
 			_ => (),
 		}
 	}
 	
 	let mut data: SpriteData = SpriteData::default();
-	let mut from_raw: bool = false;
 	
 	match parameters.source_format {
 		SpriteFormat::PNG => data = sprite_get::get_png(&parameters.source_path),
-		SpriteFormat::RAW => {
-			data = sprite_get::get_raw(&parameters.source_path);
-			from_raw = true;
-		},
+		SpriteFormat::RAW => data = sprite_get::get_raw(&parameters.source_path),
 		SpriteFormat::BIN => data = sprite_get::get_bin(&parameters.source_path),
+		SpriteFormat::BMP => data = sprite_get::get_bmp(&parameters.source_path),
 		_ => println!("main::process_file() error: Invalid source format provided"),
 	}
 	
@@ -105,52 +110,65 @@ fn process_file(parameters: Parameters) {
 	}
 	
 	match parameters.target_format {
-		SpriteFormat::PNG => sprite_make::make_png(parameters.source_path, parameters.target_path, data, parameters.palette_file, parameters.overwrite),
-		SpriteFormat::RAW => sprite_make::make_raw(parameters.source_path, parameters.target_path, data, parameters.overwrite, from_raw),
-		SpriteFormat::BIN => sprite_make::make_bin(parameters.source_path, parameters.target_path, data, parameters.uncompressed, parameters.overwrite),
+		SpriteFormat::PNG => sprite_make::make_png(parameters, data),
+		SpriteFormat::RAW => sprite_make::make_raw(parameters, data),
+		SpriteFormat::BIN => sprite_make::make_bin(parameters, data),
+		SpriteFormat::BMP => sprite_make::make_bmp(parameters, data),
 		_ => println!("main::process_file() error: Invalid target format provided"),
 	}
 }
 
 
-fn process_directory(parameters: Parameters) {
-	let mut items_processed: usize = 0;
-
-	for item in parameters.source_path.read_dir().expect("main::process_directory() error: Could not read source path") {
-		let this_path: PathBuf = item.unwrap().path();
-		
-		if !this_path.is_file() {
-			continue;
-		}
-		
-		let mut type_match: bool = false;
-		
-		match this_path.extension() {
-			Some(os_str) => match os_str.to_ascii_lowercase().to_str() {
-				Some("png") => if parameters.source_format == SpriteFormat::PNG {
-					type_match = true;
-				},
-				
-				Some("raw") => if parameters.source_format == SpriteFormat::RAW {
-					type_match = true;
-				},
-				
-				Some("bin") => if parameters.source_format == SpriteFormat::BIN {
-					type_match = true;
-				},
-				
-				_ => (),
-			}
-			_ => (),
-		}
-		
-		if type_match {
-			let mut new_params: Parameters = parameters.clone();
-			new_params.source_path = this_path;
-			process_file(new_params);
-			items_processed += 1;
-		}
+fn type_matches(extension: Option<&OsStr>, format: SpriteFormat) -> bool {
+	match extension {
+		Some(os_str) => match os_str.to_ascii_lowercase().to_str() {
+			Some("png") => return format == SpriteFormat::PNG,
+			Some("raw") => return format == SpriteFormat::RAW,
+			Some("bin") => return format == SpriteFormat::BIN,
+			Some("bmp") => return format == SpriteFormat::BMP,
+			_ => return false,
+		},
+		_ => return false,
 	}
+}
+
+
+fn process_directory_thread(mut parameters: Parameters, offset: usize) -> usize {
+	let mut items_processed: usize = 0;
+	let mut directory_items: ReadDir = parameters.source_path.read_dir().expect(
+		"main::process_directory_thread() error: Could not read source path");
+
+	for _i in 0..offset {
+		directory_items.next();
+	}
+
+	loop {
+		match directory_items.next() {
+			Some(item) => {
+				let this_path: PathBuf = item.unwrap().path();
+				
+				if type_matches(this_path.extension(), parameters.source_format) {
+					parameters.source_path = this_path;
+					process_file(parameters.clone());
+					items_processed += 1;
+				}
+			},
+			None => return items_processed,
+		}
+		directory_items.next();
+	}
+}
+
+
+// Experimental multithreading
+fn process_directory(parameters: Parameters) {
+	let params_t2: Parameters = parameters.clone();
 	
-	println!("Processed {} sprites", items_processed);
+	let handle = thread::spawn(move || process_directory_thread(params_t2, 1));
+	let item_count: usize = process_directory_thread(parameters, 0);
+	
+	// Wait for both threads to be done
+	let item_count_t2: usize = handle.join().unwrap();
+	
+	print!("Processed {} sprites", item_count + item_count_t2);
 }
